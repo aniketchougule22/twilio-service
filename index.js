f6fc3d2b743ae4fc55584e23bc84e1ce53fdbbc9
aniketchougule22 <chouguleaniket03@gmail.com>
2023-03-01 20:52:15 +0530

    Changes Done

--- index.js
+++ index.js
@@ -5,20 +5,25 @@ const twilio = (message, phone) => {
     {
         const accountSid = process.env.TWILIO_ACCOUNT_SID; 
         const authToken = process.env.TWILIO_AUTH_TOKEN; 
-        
+
         const client = require('twilio')(accountSid, authToken);
     
         client.messages
         .create({ 
             body: message, 
-            from: TWILIO_PHONE_NUMBER, 
-            messagingServiceSid: TWILIO_MESSAGING_SERVICE_ID,      
+            from: process.env.TWILIO_PHONE_NUMBER, 
+            messagingServiceSid: process.env.TWILIO_MESSAGING_SERVICE_ID,      
             to: '+91'+phone 
         }) 
-        .then(message => console.log(message.sid)).catch(err => {
+        .then(message => {
+            console.log(message.sid);
+        })
+        .catch(err => {
             console.log(err); // handle error
         })
-        .done();
+        // .done(e => {
+        //     return e;
+        // });
     }
     catch (error) 
     {
